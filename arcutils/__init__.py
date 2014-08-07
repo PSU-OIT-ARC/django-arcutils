7f7ee322cfb1071fc02b0c2663929d25332a31a7
konstantin farrell <konstantinfarrell@gmail.com>
2014-08-07 10:38:34 -0700

    Rename to arcutils; Add ChoiceEnum

--- arcutils/__init__.py
+++ arcutils/__init__.py
@@ -4,9 +4,11 @@ from django.contrib.admin.util import NestedObjects
 from django.contrib.auth.forms import PasswordResetForm
 from django.contrib.auth import get_user_model
 from django.conf import settings
+from django.utils.six import add_metaclass
+
 
 # add the arc template tags to the builtin tags, and the bootstrap tag
-add_to_builtins('arc.templatetags.arc')
+add_to_builtins('arcutils.templatetags.arc')
 add_to_builtins('bootstrapform.templatetags.bootstrap')
 
 # monkey patch the PasswordResetForm so it indicates if a user does not exist
@@ -39,3 +41,37 @@ def will_be_deleted_with(obj):
                 continue
 
         yield cls, list_of_items_to_be_deleted
+
+
+class IterableChoiceEnum(type):
+    def __iter__(self):
+        """Simply return the iterator of the _choices tuple"""
+        return iter(self._choices)
+
+
+@add_metaclass(IterableChoiceEnum)
+class ChoiceEnum(object):
+    """
+    This creates an iterable *class* (as opposed to an iterable *instance* of a
+    class). Subclasses must define a class variable called `_choices` which is a
+    list of 2-tuples. Subclasses can be passed directly to a field as the
+    `choice` kwarg.
+
+    For example:
+
+    class FooType(ChoiceEnum):
+        A = 1
+        B = 2
+
+        _choices = (
+            (A, "Alpha"),
+            (B, "Beta"),
+        )
+
+
+    class SomeModel(models.Model):
+        foo = models.ChoiceField(choices=FooType)
+    """
+    _choices = ()
+    # http://stackoverflow.com/questions/5434400/python-is-it-possible-to-make-a-class-iterable-using-the-standard-syntax
+
--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 from setuptools import setup
 
 setup(
-    name="django-arc",
+    name="django-arcutils",
     version='0.0.1',
-    url='https://github.com/PSU-OIT-ARC/django-arc',
+    url='https://github.com/PSU-OIT-ARC/django-arcutils',
     author='Matt Johnson',
     author_email='mdj2@pdx.edu',
     description="ARC Utils for Django sites",
-    packages=['arc'],
+    packages=['arcutils'],
     install_requires=[
         'django-bootstrap-form',
     ],
