3c88b176f1e59b9f1efd366262fde9c5dd14efc4
Wyatt Baldwin <wyatt.baldwin@pdx.edu>
2015-02-12 16:34:20 -0800

    Don't ignore LOGGING setting in logging.basic()
    If the LOGGING setting is set, it will be merged into the default
    logging config instead of being ignored.

--- arcutils/logging.py
+++ arcutils/logging.py
@@ -1,66 +1,106 @@
+from copy import copy
 import logging.config
+
 from . import LOGSTASH_ADDRESS
 
-def basic(_):
-    """
-    Logs to the console always, and to LOGSTASH_ADDRESS when DEBUG is off. Overrides
-    Django's logging.
 
-    To use, add to your settings file:
-    LOGGING_CONFIG = 'arcutils.logging.basic'
+LOGSTASH_HOST, LOGSTASH_PORT = LOGSTASH_ADDRESS.rsplit(':', 1)
+LOGSTASH_PORT = int(LOGSTASH_PORT)
 
-    We ignore the configuration that is passed in, hence the _ as the first
-    argument
-    """
-    logging.config.dictConfig({
-        'version': 1,
-        'disable_existing_loggers': True,
-        'formatters': {
-            'verbose': {
-                'format': '%(levelname)s %(pathname)s:%(lineno)d %(message)s'
-            }
+
+DEFAULT_CONFIG = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '[%(asctime)s] %(levelname)s %(pathname)s:%(lineno)d %(message)s',
+            'datefmt': '%d/%b/%Y %H:%M:%S',
+        }
+    },
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
         },
-        'filters': {
-            'require_debug_false': {
-                '()': 'django.utils.log.RequireDebugFalse',
-            },
-            'require_debug_true': {
-                '()': 'django.utils.log.RequireDebugTrue',
-            },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
         },
-        'handlers': {
-            'logstash': {
-                'level': 'INFO',
-                'class': 'logstash.TCPLogstashHandler',
-                'host': LOGSTASH_ADDRESS.rsplit(":", 1)[0],
-                'port': int(LOGSTASH_ADDRESS.rsplit(":", 1)[1]),
-                'version': 1,
-                'message_type': 'django',
-                'filters': ['require_debug_false'],
-            },
-            'console': {
-                'level': 'INFO',
-                'class': 'logging.StreamHandler',
-                'formatter': 'verbose'
-            },
-            'mail_admins': {
-                'level': 'ERROR',
-                'filters': ['require_debug_false'],
-                'class': 'django.utils.log.AdminEmailHandler'
-            }
-        },
-        'loggers': {
-            # we need to explicitly override the django logger so it propagates
-            # to the root logger
-            'django': {
-                'propagate': True,
-            },
-            'elasticsearch': {
-                'level': 'ERROR',
-            }
-        },
-        'root': {
-            'handlers': ['console', 'mail_admins', 'logstash'],
+    },
+    'handlers': {
+        'logstash': {
             'level': 'INFO',
+            'class': 'logstash.TCPLogstashHandler',
+            'host': LOGSTASH_HOST,
+            'port': LOGSTASH_PORT,
+            'version': 1,
+            'message_type': 'django',
+            'filters': ['require_debug_false'],
         },
-    })
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler',
+        }
+    },
+    'loggers': {
+        # we need to explicitly override the django logger so it propagates
+        # to the root logger
+        'django': {
+            'propagate': True,
+        },
+        'elasticsearch': {
+            'level': 'ERROR',
+        }
+    },
+    'root': {
+        'handlers': ['console', 'mail_admins', 'logstash'],
+        'level': 'INFO',
+    },
+}
+
+
+def basic(config):
+    """Log to the console always and to Logstash when DEBUG is off.
+
+    Overrides Django's logging.
+
+    The default config is defined in :const:`DEFAULT_CONFIG`.
+
+    To use, add the following to your settings file::
+
+        LOGGING_CONFIG = 'arcutils.logging.basic'
+
+    If you set the `LOGGING` setting (as a dict that could be passed to
+    :func:`logging.config.dictConfig`), that config will be passed to
+    this function as the ``config`` arg. Any such config will be merged
+    into `DEFAULT_CONFIG` and will take precedence over the defaults.
+    For example, to configure a logger for your app, you might set
+    `LOGGING_CONFIG` like so::
+
+        LOGGING_CONFIG = {
+            'loggers': {
+                'rethink': {
+                    'level': 'DEBUG'
+                }
+            }
+        }
+
+    """
+    config = _merge(DEFAULT_CONFIG, config)
+    return logging.config.dictConfig(config)
+
+
+def _merge(d, e):
+    """Merge dict ``e`` into dict ``d``."""
+    d = copy(d)
+    for k, v in e.items():
+        if k in d and isinstance(v, dict):
+            v = _merge(d[k], v)
+        else:
+            v = copy(v)
+        d[k] = v
+    return d
