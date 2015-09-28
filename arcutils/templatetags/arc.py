0f567d6814418f580700fbf8d132c4a3fbb71ed8
Wyatt Baldwin <wbaldwin@pdx.edu>
2015-09-28 16:07:55 -0700

    Remove unused imports

--- arcutils/templatetags/arc.py
+++ arcutils/templatetags/arc.py
@@ -1,10 +1,7 @@
 from django import template
 from django.template.defaulttags import url
-from django.core.urlresolvers import reverse
 from django.conf import settings
 from django.template import Node, Variable, VariableDoesNotExist
-from django.utils.html import conditional_escape
-from django.utils.safestring import mark_safe
 
 register = template.Library()
 
