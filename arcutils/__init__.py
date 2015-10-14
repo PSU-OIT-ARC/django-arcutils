55e70debe64ffa0980c3bb569f53e06495b7e893
Wyatt Baldwin <wbaldwin@pdx.edu>
2015-10-14 13:10:03 -0700

    Remove Django app config
    The app config was used to set things up based on the ARCUTILS_FEATURES
    setting (with fallback to DEFAULT_FEATURES). Now that all the "features"
    that needed be set up dynamically have been removed, we don't need the
    custom app config either.

--- arcutils/__init__.py
+++ arcutils/__init__.py
@@ -1,8 +1,2 @@
 from .forms import BaseFormSet, BaseModelFormSet  # noqa
 from .db import dictfetchall, will_be_deleted_with, ChoiceEnum  # noqa
-
-
-DEFAULT_FEATURES = {
-}
-
-default_app_config = 'arcutils.apps.ARCUtilsConfig'
