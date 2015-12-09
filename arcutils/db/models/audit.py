adb1f444833df0cba7f6fe0061249b815ea26791
Wyatt Baldwin <wbaldwin@pdx.edu>
2015-12-08 16:54:30 -0800

    Convert db.models into a package
    Put the AuditModel in its own module in this new package and export it
    via db.models.__init__.
    
    This is in anticipation of adding other base model classes. It would
    have been unwieldly to jam them into the same module.

--- arcutils/db/models/audit.py
+++ arcutils/db/models/audit.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 
-from ..threadlocals import get_current_user
+from arcutils.threadlocals import get_current_user
 
 
 class AuditModel(models.Model):
