6fecc53b63023e6d25722aa66038285be3b4d46b
Wyatt Baldwin <wbaldwin@pdx.edu>
2016-01-04 14:16:59 -0800

    Return just path when getting redirect location from REFERER
    In response.get_redirect_location(). There's no need to include the
    scheme and host in a redirect back to the same site. Removing them makes
    redirect URLs more concise.

--- arcutils/response.py
+++ arcutils/response.py
@@ -1,3 +1,5 @@
+from urllib.parse import urlparse, urlunparse
+
 from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.utils.http import is_safe_url
 
@@ -26,14 +28,25 @@ def get_redirect_location(request, redirect_field_name=REDIRECT_FIELD_NAME, defa
 
     """
     host = request.get_host()
-    location = (
-        request.GET.get(redirect_field_name) or
-        request.POST.get(redirect_field_name) or
-        request.META.get('HTTP_REFERER')
-    )
+    location = request.GET.get(redirect_field_name) or request.POST.get(redirect_field_name)
+
+    if location:
+        from_referrer = False
+    else:
+        location = request.META.get('HTTP_REFERER')
+        from_referrer = bool(location)
+
     if not is_safe_url(location, host):
         default = default or '/'
         if not is_safe_url(default, host):
             default = '/'
         location = default
+    elif from_referrer:
+        info = urlparse(location)
+        if info.netloc == host:
+            # Clear scheme and host (AKA netloc) to get just the path of
+            # the referrer. Also, ensure the path is set for consistency.
+            new_info = ('', '', info.path or '/') + info[3:]
+            location = urlunparse(new_info)
+
     return location
