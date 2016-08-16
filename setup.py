a79bb4a05ece27795fa49bad1deeffe4522122d4
Wyatt Lee Baldwin <wbaldwin@pdx.edu>
2016-08-16 10:36:09 -0700

    Upgrade certifi 2016.2.28 => 2016.8.8

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('VERSION') as version_fp:
 
 # Base dependencies
 install_requires = [
-    'certifi>=2016.2.28',
+    'certifi>=2016.8.8',
     'django-local-settings>=1.0b1',
     'stashward',
 ]
