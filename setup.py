bcc5de70493b6e50fe389cc2a56ed6aee6024c04
Wyatt Lee Baldwin <wbaldwin@pdx.edu>
2016-10-28 14:15:28 -0700

    Bump django-local-settings 1.0b1 => 1.0b2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('VERSION') as version_fp:
 # Base dependencies
 install_requires = [
     'certifi>=2016.9.26',
-    'django-local-settings>=1.0b1',
+    'django-local-settings>=1.0b2',
     'pytz>=2016.7',
     'raven>=5.31.0',
     'stashward',
