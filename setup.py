ad2b12951cb91092427572ae821e38d30b4f6f03
Wyatt Lee Baldwin <wbaldwin@pdx.edu>
2016-11-08 15:55:14 -0800

    Bump 3.5.1 => 3.5.3

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ else:
 
 # Dependencies that are used in multiple places
 deps = {
-    'djangorestframework': 'djangorestframework>=3.5.1',
+    'djangorestframework': 'djangorestframework>=3.5.3',
     'ldap3': 'ldap3>=1.4.0,<2.0',
 }
 
