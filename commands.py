a46d2de6bb0a9605944b44971cc29126023e0623
Wyatt Baldwin <wbaldwin@pdx.edu>
2017-06-20 11:28:34 -0700

    Add install command (import from ARCTasks)
    Because `run install -u` is so much easier to type than
    `pip install -U -r requirements.txt`.

--- commands.py
+++ commands.py
@@ -1,5 +1,5 @@
 from runcommands.commands import show_config  # noqa: F401
 
-from arctasks.base import lint  # noqa: F401
+from arctasks.base import install, lint  # noqa: F401
 from arctasks.python import show_upgraded_packages  # noqa: F401
 from arctasks.release import *  # noqa: F401,F403
